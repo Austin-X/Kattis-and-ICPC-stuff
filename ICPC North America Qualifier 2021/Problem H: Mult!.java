65bed8be0e3126d35db5e3fb4867323e17470e8b
FLAME FIRE <32133198+Austin-X@users.noreply.github.com>
2022-02-19 19:56:38 -0500

    Create Problem H: Mult!.java

---
+++ ICPC North America Qualifier 2021/Problem H: Mult!.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Mult {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		
+		int cur = -1;
+		while (n-- > 0) {
+			int x = readInt();
+			if (cur == -1) { cur = x; continue; }
+			if (x % cur == 0) { System.out.println(x); cur = -1; }
+			
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
